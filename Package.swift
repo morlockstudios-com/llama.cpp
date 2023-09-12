2d5fa7f0689d35a282c9b78db43113b644667c4c
Marc Terns <tenxurz@gmail.com>
2023-09-12 16:18:31 -0500

    Adds all the compiler flags to optimize Llama to be more performant. Also makes METAL non conditional

--- Package.swift
+++ Package.swift
@@ -1,50 +1,99 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "llama",
-    platforms: [
-        .macOS(.v12),
-        .iOS(.v14),
-        .watchOS(.v4),
-        .tvOS(.v14)
+    platforms: [.macOS(.v11),
+                .iOS(.v14),
+                .watchOS(.v4),
+                .tvOS(.v14)
     ],
     products: [
         .library(name: "llama", targets: ["llama"]),
+        .library(name: "Bert", targets: ["Bert"])
     ],
     targets: [
         .target(
             name: "llama",
             path: ".",
-            exclude: [],
             sources: [
                 "ggml.c",
                 "llama.cpp",
                 "ggml-alloc.c",
                 "ggml-backend.c",
                 "ggml-quants.c",
-                "ggml-metal.m",
+                "ggml-metal.m"
             ],
             resources: [
                 .process("ggml-metal.metal")
             ],
             publicHeadersPath: "spm-headers",
             cSettings: [
-                .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
+                .unsafeFlags(["-Wno-shorten-64-to-32",
+                              "-Ofast",
+                              "-DNDEBUG"]),
+                .define("GGML_USE_K_QUANTS"),
+                .define("GGML_USE_ACCELERATE"),
+                .define("NDEBUG"),
+                .define("_XOPEN_SOURCE", to: "600"),
+                .define("_DARWIN_C_SOURCE"),
+                .unsafeFlags(["-fno-objc-arc"]),
+                .define("GGML_SWIFT"),
+                .define("GGML_USE_METAL")
+            ],
+            cxxSettings: [
+                .unsafeFlags(["-Wno-shorten-64-to-32",
+                              "-Ofast"]),
+                .define("GGML_USE_K_QUANTS"),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
+                .define("GGML_SWIFT"),
                 .define("GGML_USE_METAL"),
-                // NOTE: NEW_LAPACK will required iOS version 16.4+
-                // We should consider add this in the future when we drop support for iOS 14
-                // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
-                // .define("ACCELERATE_NEW_LAPACK"),
-                // .define("ACCELERATE_LAPACK_ILP64")
+                .define("NDEBUG"),
+                .define("_XOPEN_SOURCE", to: "600"),
+                .define("_DARWIN_C_SOURCE")
+                ],
+            linkerSettings: [
+                .linkedFramework("Accelerate"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("Metal"),
+                .linkedFramework("MetalKit")
+            ]
+        ),
+        .target(
+            name: "Bert",
+            dependencies: [ "llama" ],
+            resources: [
+                .process("Resources")
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .define("GGML_USE_ACCELERATE"),
+                .unsafeFlags([
+                    "-Ofast", "-DNDEBUG", "-std=gnu11"
+                ])
+            ],
+            cxxSettings: [
+                .define("GGML_USE_ACCELERATE"),
+                .unsafeFlags([
+                    "-Ofast", "-DNDEBUG", "-std=gnu++20"
+                ])
             ],
             linkerSettings: [
-                .linkedFramework("Accelerate")
+                .linkedFramework("Accelerate"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("NaturalLanguage")
             ]
+        ),
+        .testTarget(name: "BertTests",
+                    dependencies: ["Bert"],
+                    resources: [
+                        .process("resources")
+                    ]
         )
     ],
+    cLanguageStandard: .c11,
     cxxLanguageStandard: .cxx11
 )
+
